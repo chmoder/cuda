c0a4ec5145f1289ccb09b44e58a65bdbad577e11
Thomas <tom.bz2@gmail.com>
2015-09-21 10:17:12 -0500

    Update PasswordChecker.cu

--- PasswordChecker/src/PasswordChecker.cu
+++ PasswordChecker/src/PasswordChecker.cu
@@ -127,7 +127,7 @@ __global__ void universalCheckPasswordShared(char *return_guess, const int strin
  * Created n streams where n = number of multiprocessors * 8 (peformance degrades after this point on my GTX)
  * thread count per kernel is your max threads / 2
  * block count is the number of multiprocessors you have
- * Using shared register memory I have been measuring about 32,499,876 password generations and comparisons per second
+ * Using shared memory and registers I have been measuring about 32,499,876 password generations and comparisons per second
  */
 char *checkPasswordHost(int iteration)
 {
