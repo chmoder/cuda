428dda2c91b5a4a4c3b4164a927ad82192ed8a10
tcross <tom.bz2@gmail.com>
2015-09-21 14:43:47 -0500

    release binary

--- PasswordChecker/Release/src/subdir.mk
+++ PasswordChecker/Release/src/subdir.mk
@@ -4,21 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CU_SRCS += \
-../src/PasswordChecker.cu 
+../src/test.cu 
 
 CU_DEPS += \
-./src/PasswordChecker.d 
+./src/test.d 
 
 OBJS += \
-./src/PasswordChecker.o 
+./src/test.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-7.5/bin/nvcc -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_20,code=sm_21 -m64 -odir "src" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-7.5/bin/nvcc -O3 --compile --relocatable-device-code=false -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_21 -m64  -x cu -o  "$@" "$<"
+	/usr/local/cuda-7.5/bin/nvcc -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_20,code=sm_21 -gencode arch=compute_52,code=sm_52  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-7.5/bin/nvcc -O3 --compile --relocatable-device-code=false -gencode arch=compute_20,code=compute_20 -gencode arch=compute_52,code=compute_52 -gencode arch=compute_20,code=sm_21 -gencode arch=compute_52,code=sm_52  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
